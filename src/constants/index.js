e8d837e7cc9da2ef5efb8164dbc53349fed5f20e
Muskan Gill <muskan4841.se24@chitkara.edu.in>
2026-02-21 20:20:24 +0530

    Done

---
+++ src/constants/index.js
@@ -0,0 +1,141 @@
+export const myProjects = [
+  {
+    id: 1,
+    title: "NexWise - AI Expense Tracker",
+    description:
+      "Manage your finances, build healthy routines, and stay organized in one powerful platform. NexWise helps students plan better, stay focused, and take control of their daily life.",
+    subDescription: [
+      "Build a scalable and modern application with React Js and python as backend to track expenses.",
+      "Implemented secure authentication using Firebase and database management using MongoDB.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "Added AI, calender, task manager and notes feature to keep all the necessary features together with tracking daily finances.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/expense.png",
+    tags: [
+      {
+        id: 1,
+        name: "Javascript",
+        path: "/assets/logos/javascript.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 2,
+    title: "Ecommerce App",
+    description:
+      "A modern and responsive E-Commerce web application built using React.js. This project focuses on frontend architecture, state management, routing, and UI design while integrating with a public fake API for product data.",
+    subDescription: [
+      "This is a frontend-focused E-Commerce application built using React. It consumes product data from the Fake Store API and demonstrates core e-commerce functionalities.",
+      "Implemented secure authentication and this project focuses on UI and frontend logic.",
+      "Designed a responsive frontend with Tailwind CSS, enhancing user experience.",
+      "It does not include real payment processing, real authentication, or backend integration.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/ecom.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+  {
+    id: 3,
+    title: "Resume Builder App",
+    description:
+      "A modern, interactive web application built for making Resume step by step using React Js.",
+    subDescription: [
+      "Developed a fully interactive Application using React that helps you make personalized Resumes with different templates.",
+      "Implemented authentication using firebase authentication with Log in with Google feature as well.",
+      "Designed responsive UI components with Tailwind CSS for an enhanced UI.",
+      "Integrated different templates and downloading Resume as pdf at the end.",
+    ],
+    href: "",
+    logo: "",
+    image: "/assets/projects/resume.png",
+    tags: [
+      {
+        id: 1,
+        name: "React",
+        path: "/assets/logos/react.svg",
+      },
+      {
+        id: 2,
+        name: "TailwindCSS",
+        path: "/assets/logos/tailwindcss.svg",
+      },
+    ],
+  },
+];
+
+export const mySocials = [
+  {
+    name: "WhatsApp",
+    href: "",
+    icon: "/assets/socials/whatsApp.svg",
+  },
+  {
+    name: "Linkedin",
+    href: "",
+    icon: "/assets/socials/linkedIn.svg",
+  },
+  {
+    name: "Instagram",
+    href: "",
+    icon: "/assets/socials/instagram.svg",
+  },
+];
+
+export const experiences = [
+  {
+    title: "Hotel Management DB",
+    job: "Database Management",
+    date: "2025",
+    contents: [
+      "SQL-based database with entities for Oberoi Hotels.",
+    ],
+  },
+  {
+    title: "Snake, Hangman, Reaction Time....",
+    job: "Introduction to Programming",
+    date: "2025",
+    contents: [
+      "Snake Game - Real-time size increasing after eating food using C++.",
+      "Reaction Time Game - C# app that measures user reaction times.",
+      "Hangman Game - Word-guessing game in C++.",
+      "Pacman Game - Classic maze game in C++.",
+    ],
+  },
+  {
+    title: "HTML, CSS, Javascript",
+    job: "Full Stack Developer",
+    date: "2025",
+    contents: [
+      "Flower Fortune Predictor - Web app using HTML/CSS/JS to display random fortunes based on button clicks.",
+      "Rock Paper Scissors - Wep app using HTML, CSS, Js to play rock paper sci with computer online.",
+    ],
+  },
+  {
+    title: "Research Related Work",
+    job: "Academic",
+    date: "2025",
+    contents: [
+      "AI Journal App: Documentation of proposed SDLC-based app with full analysis and design.",
+      "Blockchain in Military Communication: Mission-critical blockchain research system.",
+    ],
+  },
+];
